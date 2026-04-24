6962178b55ec452d34c44a27ec51ce8ac84034e3
mwmartella <mwmartella@gmail.com>
2026-04-24 10:24:45 +0800

    update Worker model to use UUIDs and add start/end date fields

--- app/models.py
+++ app/models.py
@@ -1,17 +1,26 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Date, Uuid
 from app.db import Base
+import uuid6
 
 
 class Worker(Base):
     __tablename__ = "workers"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid6.UUID] = mapped_column(
+        Uuid(as_uuid=True), primary_key=True, default=uuid6.uuid7
+    )
 
-    worker_code: Mapped[str] = mapped_column(String(50), unique=True)
+    worker_code: Mapped[str] = mapped_column(String(50))
+    # our farm's code for the worker type, ie. C2
+    # this will be FKed to the worker code table when its built.
 
     first_name: Mapped[str] = mapped_column(String(100))
 
     last_name: Mapped[str] = mapped_column(String(100))
 
-    active: Mapped[bool] = mapped_column(Boolean, default=True)
+    start_date: Mapped[Date] = mapped_column(Date, nullable=False)
+
+    end_date: Mapped[Date | None] = mapped_column(Date, nullable=True)
+    # When end_date is NULL the worker is active.
+    # When end_date is set the worker is no longer active.
